370be5b70559e77c667e0fe4cf9cd057ffaeb26c
tmduddy <login@tylerduddy.com>
2021-12-12 11:48:53 -0500

    start day 4 add helper script

---
+++ days/day4.py
@@ -0,0 +1,18 @@
+from utils.submarine import Submarine
+
+TEST_PATH = "inputs/tests/day4.csv"
+FILE_PATH = "inputs/day4.csv"
+
+submarine = Submarine()
+
+def day4a():
+    print('--- DAY 4A ---')
+    
+
+def day4b():
+    print('--- DAY 4B ---')
+
+
+if __name__ == '__main__':
+    day4a() # answer = 
+    day4b() # answer = 
---
+++ make_day.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+
+DAY_NUMBER=$1
+DATA_FILE_NAME="day${DAY_NUMBER}.csv"
+SCRIPT_NAME="day${DAY_NUMBER}.py"
+
+# touch inputs/$DATA_FILE_NAME
+# touch inputs/tests/$DATA_FILE_NAME
+touch days/$SCRIPT_NAME
+
+cat <<EOF > days/$SCRIPT_NAME
+from utils.submarine import Submarine
+
+TEST_PATH = "inputs/tests/$DATA_FILE_NAME"
+FILE_PATH = "inputs/$DATA_FILE_NAME"
+
+submarine = Submarine()
+
+def day${DAY_NUMBER}a():
+    print('--- DAY ${DAY_NUMBER}A ---')
+    
+
+def day${DAY_NUMBER}b():
+    print('--- DAY ${DAY_NUMBER}B ---')
+
+
+if __name__ == '__main__':
+    day${DAY_NUMBER}a() # answer = 
+    day${DAY_NUMBER}b() # answer = 
+EOF
