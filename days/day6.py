b3beb0fd7ecbeb0e382a260a8f420c9633a37a95
tmduddy <login@tylerduddy.com>
2021-12-14 20:30:27 -0500

    start day 6

---
+++ days/day6.py
@@ -0,0 +1,18 @@
+from utils.submarine import Submarine
+
+TEST_PATH = "inputs/tests/day6.csv"
+FILE_PATH = "inputs/day6.csv"
+
+submarine = Submarine()
+
+def day6a():
+    print('--- DAY 6A ---')
+    
+
+def day6b():
+    print('--- DAY 6B ---')
+
+
+if __name__ == '__main__':
+    day6a() # answer = 
+    day6b() # answer = 
