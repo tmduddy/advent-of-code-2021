9cc3eb5653a62777979130fed3618e874c4ae5f1
Tyler Duddy <tyler.duddy@newtonx.com>
2021-12-24 10:15:21 -0500

    finish day 6

--- days/day6.py
+++ days/day6.py
@@ -4,15 +4,21 @@ from copy import deepcopy
 TEST_PATH = "inputs/tests/day6.csv"
 FILE_PATH = "inputs/day6.csv"
 
-def day6a():
-    print('--- DAY 6A ---')
+def load_data():
     with open(FILE_PATH, 'r') as f:
         reader = csv.reader(f)
         data = list(map(int,[row for row in reader][0]))
+    return data
 
-    days = 80
+def day6a():
+    print('--- DAY 6A ---')
+    data = load_data()
+    data = [0]
+
+    days = 156
     output = deepcopy(data)
-    for _ in range(days):
+    print(f'day 0: {output}')
+    for day in range(days):
         for i, value in enumerate(data):
             if value == 0:
                 output[i] = 6
@@ -20,13 +26,24 @@ def day6a():
             else:
                 output[i] -= 1
         data = deepcopy(output)
-        # print(data)
+        # print(f'day {day+1}: {data}')
+        print(f'day {day+1}: {len(data)}')
     print(len(data))
 
 def day6b():
     print('--- DAY 6B ---')
+    days = 256
+    data = load_data()
+    fish = [data.count(i) for i in range(9)]
+    
+    for _ in range(days):
+        num = fish.pop(0)
+        fish[6] += num
+        fish.append(num)
+    
+    print(sum(fish))
 
 
 if __name__ == '__main__':
     # day6a() # answer = 363101
-    day6b() # answer = 
+    day6b() # answer = 1644286074024
